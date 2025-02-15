d64114fdb752880d12dc7641c58a1f910594b297
Cryspysam <ssmishra9918@gmail.com>
2025-02-15 20:09:07 +0530

    Updated features or bug fixes

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-import { faker } from '@faker-js/faker';
+import mysql from "mysql2";
+import { faker } from "@faker-js/faker";
 
-let getRandomUser = () => ({
-    id: faker.string.uuid(),  // Use faker.string.uuid() instead of faker.datatype.uuid()
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "delta_app",
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error("MySQL connection failed: ", err);
+        return;
+    }
+    console.log("Connected to MySQL successfully!");
+
+    connection.query("SHOW TABLES", (err, result) => {
+        if (err) {
+            console.error("Error fetching tables: ", err);
+        } else {
+            console.log("Tables: ", result);
+        }
+
+        connection.end();
+    });
+});
+
+const getRandomUser = () => ({
+    id: faker.string.uuid(),
     username: faker.internet.userName(),
     email: faker.internet.email(),
     password: faker.internet.password(),
 });
 
-console.log(getRandomUser());
+console.log("Generated User: ", getRandomUser());
